48f91c9229689ba64397d8e3147181a6cfde155e
Brandon Hall <bran.tyler.hall@gmail.com>
2023-01-26 16:37:33 -0500

    break

---
+++ update_exercises.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+# SELECT album_name AS 'All albums in the table' FROM albums;
+# SELECT album_name AS 'Albums released before 1980' FROM albums WHERE release_date_year < 1980;
+# SELECT album_name AS 'All Michael Jackson Albums' FROM albums WHERE artist_name = 'Michael Jackson';
+
