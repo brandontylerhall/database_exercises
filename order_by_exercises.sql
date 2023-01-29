d9403b8672218de4513c1cdeeae58e66c528598f
Brandon Hall <bran.tyler.hall@gmail.com>
2023-01-29 12:20:35 -0500

    finished order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,18 +1,16 @@
 USE employees;
 
-# SELECT emp_no, first_name
+# SELECT emp_no, first_name, last_name
 # FROM employees
 # WHERE gender = 'M'
-#   AND (
-#             first_name = 'Irena'
-#         OR first_name = 'Vidya'
-#         OR first_name = 'Maya'
-#     );
+#  AND first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY last_name, first_name;
 
 # SELECT emp_no, last_name
 # FROM employees
 # WHERE last_name LIKE 'E%'
-#    AND last_name LIKE '%e';
+#    AND last_name LIKE '%e'
+# ORDER BY emp_no DESC;
 
 # SELECT emp_no, last_name
 # FROM employees
